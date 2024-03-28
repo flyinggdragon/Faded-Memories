45ff72cbcc812a3994d1404bb2669cc47c6b6bd9
gairin <marco14963@gmail.com>
2024-03-28 13:42:12 -0300

    Diálogo com escolhas!!

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour {
+    [SerializeField] private GameObject dialogueParent;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField] private Button option1Button;
+    [SerializeField] private Button option2Button;
+
+    [SerializeField] private float typingSpeed = 0.05f;
+    // [SerializeField] private float turnSpeed = 2f;
+
+    private List<DialogueString> dialogueList;
+
+    [Header("Player")]
+    // parar movimento
+
+    private int currentDialogueIndex = 0;
+
+    private void Start() {
+        dialogueParent.SetActive(false);
+    }
+
+    public void DialogueStart(List<DialogueString> textToPrint) {
+        dialogueParent.SetActive(true);
+        //disable player control
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        dialogueList = textToPrint;
+        currentDialogueIndex = 0;
+
+        DisableButtons();
+        StartCoroutine(PrintDialogue());
+    }
+
+    private void DisableButtons() {
+        option1Button.interactable = false;
+        option2Button.interactable = false;
+
+        option1Button.GetComponentInChildren<TMP_Text>().text = "No Option";
+        option2Button.GetComponentInChildren<TMP_Text>().text = "No Option";
+    }
+    
+    private bool optionSelected = false;
+
+    private IEnumerator PrintDialogue() {
+        while (currentDialogueIndex < dialogueList.Count) {
+            DialogueString line = dialogueList[currentDialogueIndex];
+
+            line.startDialogueEvent?.Invoke();
+
+            if (line.isQuestion) {
+                yield return StartCoroutine(TypeText(line.text));
+            
+                option1Button.interactable = true;
+                option2Button.interactable = true;
+
+                option1Button.GetComponentInChildren<TMP_Text>().text = line.answerOption1;
+                option2Button.GetComponentInChildren<TMP_Text>().text = line.answerOption2;
+            
+                option1Button.onClick.AddListener(() => HandleOptionSelected(line.option1IndexJump));
+                option2Button.onClick.AddListener(() => HandleOptionSelected(line.option2IndexJump));
+            
+                yield return new WaitUntil(() => optionSelected);
+            }
+
+            else {
+                yield return StartCoroutine(TypeText(line.text));
+            }
+
+            line.endDialogueEvent?.Invoke();
+            optionSelected = false;
+        }
+
+        DialogueStop();
+    }
+
+    private void HandleOptionSelected(int indexJump) {
+        optionSelected = true;
+        DisableButtons();
+
+        currentDialogueIndex = indexJump;
+    }
+
+    private IEnumerator TypeText(string text) {
+        dialogueText.text = "";
+
+        foreach (char letter in text.ToCharArray()) {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        if (!dialogueList[currentDialogueIndex].isQuestion) {
+            yield return new WaitUntil(() => Input.GetMouseButtonDown(0));
+        }
+
+        if (dialogueList[currentDialogueIndex].isEnd) {
+            DialogueStop();
+        }
+
+        currentDialogueIndex++;
+    }
+
+    private void DialogueStop() {
+        StopAllCoroutines();
+        dialogueText.text = "";
+        dialogueParent.SetActive(false);
+
+        //disable player control
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DialogueTrigger : MonoBehaviour {
+    [SerializeField] private List<DialogueString> dialogueStrings = new List<DialogueString>();
+
+    private bool hasSpoken = false;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player") && !hasSpoken) {
+            other.gameObject.GetComponent<DialogueManager>().DialogueStart(dialogueStrings);
+
+            hasSpoken = true;
+        }
+    }
+}
+
+[System.Serializable]
+public class DialogueString {
+    public string text;
+    public bool isEnd;
+    
+    [Header("Branch")]
+    public bool isQuestion;
+    public string answerOption1;
+    public string answerOption2; 
+    public int option1IndexJump;
+    public int option2IndexJump;
+
+    [Header("Triggered Events")]
+    public UnityEvent startDialogueEvent;
+    public UnityEvent endDialogueEvent;
+}
