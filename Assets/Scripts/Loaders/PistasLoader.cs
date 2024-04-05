73f8fba0e4a045b53320e948977186099db206cf
gairin <marco14963@gmail.com>
2024-04-05 11:40:25 -0300

    Grid Pistas

--- Assets/Scripts/Loaders/PistasLoader.cs
+++ Assets/Scripts/Loaders/PistasLoader.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PistasLoader : MonoBehaviour {
     public ItemLoader.ItemList itemList;
@@ -10,12 +11,22 @@ public class PistasLoader : MonoBehaviour {
 
         foreach (ItemLoader.Item item in itemList.item) {
             Sprite sprite = Resources.Load<Sprite>("Sprites/" + item.fileName);
+            
+            GameObject obj = new GameObject("Cell " + item.name);
+            obj.transform.SetParent(this.transform);
+            
+            RectTransform rectTransform = obj.AddComponent<RectTransform>();
 
-            GameObject obj = new GameObject(item.name);
-            SpriteRenderer spriteRenderer = obj.AddComponent<SpriteRenderer>();                // Define o sprite carregado como o sprite do SpriteRenderer
-            spriteRenderer.sprite = sprite;
-            obj.transform.position = transform.position;
-            obj.transform.parent = transform;
+            rectTransform.anchoredPosition = Vector2.zero; 
+
+            Image image = obj.AddComponent<Image>();
+            image.sprite = sprite;
+
+            float width = sprite.rect.width;
+            float height = sprite.rect.height;
+            float aspectRatio = width / height;
+
+            rectTransform.sizeDelta = new Vector2(rectTransform.sizeDelta.y * aspectRatio, rectTransform.sizeDelta.y);
         }
     }
 }
