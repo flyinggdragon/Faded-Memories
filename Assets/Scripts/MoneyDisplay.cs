f9578461d140c0f8557d471d2a4dc1a4f00e7816
gairin <marco14963@gmail.com>
2024-06-10 16:25:35 -0300

    Sistema de Dinheiro in-game
    Também reorganização de arquivos e exclusão de cenas desnecessáriass

---
+++ Assets/Scripts/MoneyDisplay.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyDisplay : MonoBehaviour {
+    private TMP_Text txtObj;
+
+    void Start() {
+        txtObj = GetComponentInChildren<TMP_Text>();
+    }
+
+    void Update() {
+        txtObj.text = $"{GameManager.money}";
+    }
+}
