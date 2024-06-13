e490686e5ab1df00322592be42bdf6e4523e344e
gairin <marco14963@gmail.com>
2024-06-12 23:31:16 -0300

    Melhorada a aparência dos objetivos, um pouco

--- Assets/Scripts/Managers/ObjectivesManager.cs
+++ Assets/Scripts/Managers/ObjectivesManager.cs
@@ -6,8 +6,11 @@ using TMPro;
 
 public class ObjectivesManager : MonoBehaviour {
     private List<Objective> objectives = new List<Objective>();
-    private List<ObjectiveItem> objectivesList = new List<ObjectiveItem>();
+    private List<ObjectiveItem> objectiveItems = new List<ObjectiveItem>();
     private GameObject objectiveContainer;
+    private GameObject currentObjective;
+
+    private GameObject objectiveItemPrefab;
 
     public static List<Objective> LoadObjectiveList(string path) {
         string jsonText = File.ReadAllText(path);
@@ -21,16 +24,40 @@ public class ObjectivesManager : MonoBehaviour {
         objectives = LoadObjectiveList("Assets/GameData/Objectives.json");
 
         objectiveContainer = transform.GetChild(1).gameObject;
+        currentObjective = transform.GetChild(0).gameObject; 
+        
+        objectiveItemPrefab = Resources.Load<GameObject>("Prefabs/ObjectiveItemPrefab");
+
+        NewObjective(objectives[0]);
+        objectives.Remove(objectives[0]);
 
         foreach (Objective objective in objectives) {
             if (objective == GameManager.currentObjective) {
                 continue;
             }
 
-            ObjectiveItem objItem = new ObjectiveItem(objective);
+            ObjectiveItem objItem = new ObjectiveItem(objective, objectiveItemPrefab);
             objItem.obj.transform.SetParent(objectiveContainer.transform, false);
-            objectivesList.Add(objItem);
+            objectiveItems.Add(objItem);
         }
+    }
+
+    private void NewObjective(Objective newObjective) {
+        TMP_Text title = currentObjective.transform.GetChild(0).GetComponent<TMP_Text>();
+        TMP_Text description = currentObjective.transform.GetChild(1).GetComponent<TMP_Text>();
+
+        title.text = newObjective.title;
+        description.text = newObjective.description;
+
+        newObjective.active = true;
+    }
+
+    private void FinishObjective(Objective current) {
+        current.finished = true;
+        current.active = false;
+
+        objectives.Remove(current);
+        NewObjective(objectives[0]);
     }
 
     [System.Serializable]
@@ -47,25 +74,22 @@ public class ObjectivesManager : MonoBehaviour {
         public TMP_Text title;
         public TMP_Text description;
 
-        public ObjectiveItem(Objective p_objective) {
+        public ObjectiveItem(Objective p_objective, GameObject prefab) {
             objective = p_objective;
-            obj = GenObjectItem(p_objective.title, p_objective.description);
+            obj = GenObjectItem(prefab, p_objective.title, p_objective.description);
         }
 
-        private GameObject GenObjectItem(string name, string desc) {
-            GameObject obj = new GameObject(name);
+        private GameObject GenObjectItem(GameObject prefab, string name, string desc) {
+            GameObject obj = GameObject.Instantiate(prefab);
+            obj.name = name;
 
-            GameObject titleObj = new GameObject("Title");
-            TMP_Text titleText = titleObj.AddComponent<TextMeshProUGUI>();
-            titleText.text = name;
-            titleObj.transform.SetParent(obj.transform, false);
-            title = titleText;
+            title = obj.transform.GetChild(0).GetComponent<TMP_Text>();
+            description = obj.transform.GetChild(1).GetComponent<TMP_Text>();
 
-            GameObject descObj = new GameObject("Description");
-            TMP_Text descText = descObj.AddComponent<TextMeshProUGUI>();
-            descText.text = desc;
-            descObj.transform.SetParent(obj.transform, false);
-            description = descText;
+            title.text = name;
+            description.text = desc;
+
+            obj.AddComponent<RectTransform>();
 
             return obj;
         }
