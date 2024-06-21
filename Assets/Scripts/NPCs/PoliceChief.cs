74983aa676423981b734bd6538d3232a2231e982
gairin <marco14963@gmail.com>
2024-06-20 22:17:20 -0300

    Police Chief adicionado

---
+++ Assets/Scripts/NPCs/PoliceChief.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceChief : NPC {
+    public override string npcName { get; set; } = "Police Chief";
+    public override void RevealName() {}
+}
