871948a60c963807970918dd1238d704bd36509a
BOrlandi69 <143264400+BOrlandi69@users.noreply.github.com>
2024-06-28 13:52:49 -0300

    Nome dos lugares adicionado

---
+++ Assets/Scripts/LocationName.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class LocationName : MonoBehaviour 
+{
+
+ public GameObject canvasWindow;
+   void Start () 
+   {    
+        //canvasWindow.sizeDelta = new Vector2(canvasText1.preferredWidth > 200 ? 200 : canvasText1.preferredWidth, canvasText1.preferredHeight);
+        Invoke("DisableText", 3.5f);//invoke after 5 seconds
+   }
+   void DisableText()
+   { 
+      canvasWindow.SetActive(false);
+   }    
+}
